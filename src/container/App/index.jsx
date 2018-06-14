aa82f79fc689190dd169aa7e9b81a5e264429a7c
Shashank <shashank.keshava@bookmyshow.com>
2018-06-14 12:23:44 +0530

    Edited Mock Fetch Call

--- src/container/App/index.jsx
+++ src/container/App/index.jsx
@@ -1,75 +1,74 @@
 import * as React from 'react';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
 
-import FormField from '../../components/FormField'
-import Loader from '../../components/Loader'
-import SnackBar from '../../components/SnackBar'
-import {sendFormData, formSubmitSuccess} from '../../actions'
+import FormField from '../../components/FormField';
+import Loader from '../../components/Loader';
+import SnackBar from '../../components/SnackBar';
+import { sendFormData, formSubmitSuccess } from '../../actions';
 
 import './App.css';
 
+
+/**
+ * @description Mock Fetch API Call
+ * @returns resolve after 2000 ms 
+ */
+const mockFetchCall = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Success');
+        }, 2000);
+    });
+
 class App extends React.Component {
 
-  mockFetchCall = () => {
-    setTimeout(() => {
-      this
-        .props
-        .formSubmitSuccess()
-    }, 10000);
-  }
+    handleSubmit = () => {
+        this.props.sendFormData();
+        // Mock Fetch Call
+        mockFetchCall().then(() => {
+            console.log('API CAll success');
+            this.props.formSubmitSuccess();
+        });
+    };
 
-  handleSubmit = () => {
-    this
-      .props
-      .sendFormData()
-    // Mock Fetch Call
-    this.mockFetchCall();
-  }
-
-  render() {
-    const {disable, apiSuccess} = this.props;
-    return (
-      <div className="App">
-        <div className="App-header">{"Jimdo Form Trial Project"}</div>
-        <div className="App-body">
-          {disable &&< Loader />}
-          <form>
-            {this
-              .props
-              .form
-              .map((field, index) =>< FormField key = {
-                index
-              }
-              type = {
-                field
-              }
-              label = {
-                field.label
-              } />)}
-          </form>
-          <button onClick={() => this.handleSubmit()}>
-            Submit
-          </button>
-        </div>
-        {apiSuccess &&< SnackBar isSnackBarOpen = {
-          apiSuccess
-        } />}
-      </div>
-    );
-  }
+    render() {
+        const { disable, apiSuccess } = this.props;
+        return (
+            <div className="App">
+                <div className="App-header">{'Form Trial Project'}</div>
+                <div className="App-body">
+                    {disable && <Loader />}
+                    <form>
+                        {this.props.form.map((field, index) => (
+                            <FormField
+                                key={index}
+                                type={field}
+                                label={field.label}
+                            />
+                        ))}
+                    </form>
+                    <button onClick={() => this.handleSubmit()}>Submit</button>
+                </div>
+                {apiSuccess && <SnackBar isSnackBarOpen={apiSuccess} />}
+            </div>
+        );
+    }
 }
 
 const mapToStateToProps = state => {
-  return state;
-}
+    return state;
+};
 
 const mapDispatchToProps = dispatch => {
-  return {
-    sendFormData: () => dispatch(sendFormData()),
-    formSubmitSuccess: () => dispatch(formSubmitSuccess())
-  }
-}
+    return {
+        sendFormData: () => dispatch(sendFormData()),
+        formSubmitSuccess: () => dispatch(formSubmitSuccess()),
+    };
+};
 
-const appVisibility = connect(mapToStateToProps, mapDispatchToProps)(App)
+const appVisibility = connect(
+    mapToStateToProps,
+    mapDispatchToProps
+)(App);
 
 export default appVisibility;
